499fc35fccdd587cc4e1bb61e7d53d93809b71c0
dionisos <denis.baudouin@gmail.com>
2017-03-08 17:53:21 +0100

    Create SuperPattern class

--- perm-alone-super-pattern.js
+++ perm-alone-super-pattern.js
@@ -1,3 +1,43 @@
+function SuperPattern(pattern) {
+  this.pattern = pattern;
+
+  this.nextPattern = function () {
+    var i = 0;
+    var newPattern = [];
+    var aNumber = 0;
+
+    for (i = this.pattern.length - 1; i >= 0; i--) {
+      if (this.pattern[i] >= 2 ) {
+        if (this.pattern[i] === 2) {
+          if(i === 0) {
+            return null;
+          }
+          newPattern = this.pattern.slice(0, i).concat([this.pattern[i]-1]).concat(this.pattern.slice(i+1)).concat([1]);
+        } else {
+          if(1 + aNumber < this.pattern[i]) {
+            newPattern = this.pattern.slice(0, i).concat([this.pattern[i]-1]).concat([1 + aNumber]);
+          } else {
+            newPattern = this.pattern.slice(0, i).concat([this.pattern[i]-1]).concat(this.pattern[i]-1).concat(this.pattern.slice(i-1+this.pattern[i]));
+          }
+        }
+        return new SuperPattern(newPattern);
+      } else {
+        aNumber++;
+      }
+    }
+    return null;
+  };
+}
+
 function permAloneSuperPattern(str) {
-  return 10;
+  superPattern = new SuperPattern([9]);
+
+  while (superPattern !== null) {
+    console.log(superPattern.pattern.toString());
+    superPattern = superPattern.nextPattern();
+  }
+  // for(var i=0; i<4; i++) {
+  //   console.log(superPattern.pattern.toString());
+  //   superPattern = superPattern.nextPattern();
+  // }
 }
