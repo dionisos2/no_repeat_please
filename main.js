4ae1d0eeb8a63ac47401204571caaba8a5bfc41f
dionisos <denis.baudouin@gmail.com>
2017-03-08 16:25:31 +0100

    Initialize env to create permAloneSuperPattern

--- main.js
+++ main.js
@@ -3,4 +3,10 @@ window.onload = function () {
   // tests();
   // benchmarks();
   createPermTab();
+
+  var result = "";
+  var resultSpan = document.getElementById('result');
+  result += "aaaxxxx : " + permAloneSuperPattern("aaaxxxx").toString() + "</br>";
+  resultSpan.innerHTML = result;
+  permAloneSuperPattern("aaaxxxx");
 };
